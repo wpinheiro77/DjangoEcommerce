fecbd7c7fcaba752540746e9b51744dcaa0f6141
wpinheiro <bocagames@yahoo.com.br>
2018-10-09 21:44:06 -0300

    Primeiro commit - Ecommerce

---
+++ djangoecommerce/urls.py
@@ -0,0 +1,27 @@
+"""djangoecommerce URL Configuration
+
+The `urlpatterns` list routes URLs to views. For more information please see:
+    https://docs.djangoproject.com/en/2.1/topics/http/urls/
+Examples:
+Function views
+    1. Add an import:  from my_app import views
+    2. Add a URL to urlpatterns:  path('', views.home, name='home')
+Class-based views
+    1. Add an import:  from other_app.views import Home
+    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
+Including another URLconf
+    1. Import the include() function: from django.urls import include, path
+    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
+"""
+from django.contrib import admin
+from django.urls import path, re_path
+
+from core import views
+
+urlpatterns = [
+    re_path(r'^$', views.index, name='index'),
+    re_path(r'^contato$', views.contact, name='contact'),
+    re_path(r'^produto$', views.product, name='product'),
+    re_path(r'^produtos$', views.product_list, name='product_list'),
+    re_path(r'^admin/', admin.site.urls),
+]
